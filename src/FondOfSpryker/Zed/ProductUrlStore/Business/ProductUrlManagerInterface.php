0749e3dabcecb88c7a8f397e4d156e1713e2ab82
Jozsef Geng <gengjozsef86@gmail.com>
2019-02-15 10:25:11 +0100

    add Manager Interface

---
+++ src/FondOfSpryker/Zed/ProductUrlStore/Business/ProductUrlManagerInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FondOfSpryker\Zed\ProductUrlStore\Business;
+
+use Spryker\Zed\Product\Business\Product\Url\ProductUrlManagerInterface as SprykerProductUrlManagerInterface;
+
+interface ProductUrlManagerInterface extends SprykerProductUrlManagerInterface
+{
+
+}
